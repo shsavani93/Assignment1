c85ebff678694f8a2c203f0909962c90b1cf7e87
shsavani93 <79660358+shsavani93@users.noreply.github.com>
2021-02-26 13:09:11 -0700

    Swapping code for methods
    Moved code from reverseNumber  method to factorial method and vice versa.

--- GitDemo.java
+++ GitDemo.java
@@ -23,12 +23,9 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 0;
-
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+        int factorialNumber = 1;
+        for(int i = 1; i <= num; ++i){
+            factorialNumber = factorialNumber * i;
         }
 
         return factorialNumber;
@@ -40,13 +37,16 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
-        double newNumber = 1;
+        int newNumber = 0;
 
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
+        while( num != 0 ) {
+            int temp = num % 10;
+            newNumber = newNumber * 10 + temp;
+            num /= 10;
         }
 
-        return newNumber;
+        reverseNum = newNumber;
+        return reverseNum;
     }
 
 }
